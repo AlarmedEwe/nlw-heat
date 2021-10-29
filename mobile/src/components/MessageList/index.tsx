391072f9d71332cd49bb74adcf90f568fa8c1971
AlarmedEwe <guilherme11do9@gmail.com>
2021-10-28 22:19:06 -0300

    Enviando e recebendo mensagens

--- mobile/src/components/MessageList/index.tsx
+++ mobile/src/components/MessageList/index.tsx
@@ -1,27 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView } from "react-native";
-import { Message } from "../Message";
+import { io } from "socket.io-client";
+import { api } from "../../services/api";
+import { Message, MessageProps } from "../Message";
 import { styles } from "./style";
 
-const msg = {
-	id: "1",
-	text: "Mensagem",
-	user: {
-		name: "Ana",
-		avatar_url: "",
-	},
-};
+let messagesQueue: MessageProps[] = [];
+
+const socket = io(String(api.defaults.baseURL));
+
+socket.on("new_message", (newMessage) => {
+	messagesQueue.push(newMessage);
+});
 
 export const MessageList = () => {
+	const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
+
+	useEffect(() => {
+		async function fetchMessages() {
+			const messageResponse = await api.get("/messages/last3");
+			setCurrentMessages(messageResponse.data);
+		}
+
+		fetchMessages();
+	}, []);
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			if (messagesQueue.length > 0) {
+				setCurrentMessages((prevState) => [
+					messagesQueue[0],
+					prevState[0],
+					prevState[1],
+				]);
+
+				messagesQueue.shift();
+			}
+		}, 3000);
+
+		return () => clearInterval(timer);
+	}, []);
+
 	return (
 		<ScrollView
 			style={styles.container}
 			contentContainerStyle={styles.content}
 			keyboardShouldPersistTaps="never"
 		>
-			<Message data={msg} />
-			<Message data={msg} />
-			<Message data={msg} />
+			{currentMessages.map((message) => (
+				<Message key={message.id} data={message} />
+			))}
 		</ScrollView>
 	);
 };
